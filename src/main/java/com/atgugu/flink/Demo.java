6fc171d8511da8cf46348c79d2b0844d2dfc3e7b
lzc <lzc@atguigu.cn>
2022-04-02 15:12:45 +0800

    update

---
+++ src/main/java/com/atgugu/flink/Demo.java
@@ -0,0 +1,11 @@
+package com.atgugu.flink;
+
+/**
+ * @Author lzc
+ * @Date 2022/4/2 15:09
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+    }
+}
